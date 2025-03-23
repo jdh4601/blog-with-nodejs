55285755acac6629feef8a3515382eaf6c1820eb
DongHyun Jung <jdh9490@gmail.com>
2025-03-23 12:25:05 +0900

    edit resume data

--- server/config/resume-data.js
+++ server/config/resume-data.js
@@ -1,87 +1,82 @@
 const resumeData = {
-  "name": "Donghyun Jeong",
-  "email": "jdh9490@gmail.com",
-  "phone": "(+82) 010-4601-9782",
-  "location": "Seoul, South Korea",
-  "Interests": "Computer Vision, Vision Transformer (ViT), Software Engineering",
-  "experiences": [
+  name: 'Donghyun Jeong',
+  email: 'jdh9490@gmail.com',
+  phone: '(+82) 010-4601-9782',
+  location: 'Seoul, South Korea',
+  Interests: 'Computer Vision, Vision Transformer (ViT), Software Engineering',
+  experiences: [
     {
-      "title": "Undergraduate Researcher (Computer Vision Lab)",
-      "company": "Incheon National University",
-      "startDate": "Feb 2025",
-      "endDate": "Present",
-      "responsibilities": [
-        "Conducting research on Vision Transformers (ViT), focusing on applying transformer-based architectures to enhance visual understanding tasks.",
-        "Implementing and optimizing transformer-based architectures using PyTorch/TensorFlow."
-      ]
-    },
-    {
-      "title": "ESG College fellowship",
-      "company": "Hyundai Motor Chung Mong-Koo Foundation (part-time)",
-      "startDate": "Jul 2022",
-      "endDate": "Oct 2022",
-      "responsibilities": [
-        "Conducted research on global issues, including climate-tech, AI, and business, while engaging with experts to gain deeper insights.",
-        "Delivered team presentations on solutions for equitable access to stock market information, with a focus on ESG principles."
-      ]
-    },
-    {
-      "title": "Class of 2022",
-      "company": "Likelion (part-time)",
-      "startDate": "Mar 2022",
-      "endDate": "Dec 2022",
-      "responsibilities": [
-        "Proficient in FE development (HTML, CSS, JavaScript, React.js)",
-        "Experienced with Figma for wireframing and prototyping.",
-        "Built responsive websites and web apps, focusing on user-friendly interfaces and optimized performance."
-      ]
-    }
-  ],
-  "projects": [
-    {
-      "name": "[Hyundai Mobis] Vehicle-to-vehicle electricity sharing platform service",
-      "startDate": "Dec 2022",
-      "endDate": "Feb 2023",
-      "link": "",
-      "description": [
-        "Collaborated in a cross-functional team to develop a car-sharing solution using a map API for locating nearby vehicles.",
-        "Designed and implemented responsive React components to enhance user experience.",
-        "Contributed to the review registration and feedback feature for user engagement.",
-        "Focused on user-centered UI design principles."
+      title: 'Undergraduate Researcher (Computer Vision Lab)',
+      company: 'Incheon National University',
+      startDate: 'Feb 2025',
+      endDate: 'Present',
+      responsibilities: [
+        'Engaged in research on advanced object detection techniques using event-based camera systems.',
       ],
-      "technologies": [
-        "React",
-        "React-query",
-        "Figma"
-      ]
-    }
-  ],
-  "education": [
-    {
-      "institution": "Incheon National University",
-      "degree": "Bachelor of Computer Science",
-      "startDate": "Mar 2022",
-      "endDate": "Present",
-      "gpa": "",
-      "logo": ""
-    }
-  ],
-  "certifications": [
-    {
-      "name": "Machine Learning Specialization",
-      "organization": "Deeplearning.AI",
-      "dateEarned": "Dec 2024",
-      "validUntil": "",
-      "link": "https://coursera.org/share/e602ff745bdf81641b96c76f96cb36a0"
     },
     {
-      "name": "AWS Certified Cloud Practitioner",
-      "organization": "Amazon Web Services",
-      "dateEarned": "Oct 2024",
-      "validUntil": "",
-      "link": "https://www.credly.com/badges/7a04f09e-5e11-42b6-838e-ee5af58ec955/public_url"
-    }
-  ]
+      title: 'ESG College fellowship',
+      company: 'Hyundai Motor Chung Mong-Koo Foundation (part-time)',
+      startDate: 'Jul 2022',
+      endDate: 'Oct 2022',
+      responsibilities: [
+        'Conducted research on global issues, including climate-tech, AI, and business, while engaging with experts to gain deeper insights.',
+        'Delivered team presentations on solutions for equitable access to stock market information, with a focus on ESG principles.',
+      ],
+    },
+    {
+      title: 'Class of 2022',
+      company: 'Likelion (part-time)',
+      startDate: 'Mar 2022',
+      endDate: 'Dec 2022',
+      responsibilities: [
+        'Proficient in FE development (HTML, CSS, JavaScript, React.js)',
+        'Experienced with Figma for wireframing and prototyping.',
+        'Built responsive websites and web apps, focusing on user-friendly interfaces and optimized performance.',
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: '[Hyundai Mobis] Vehicle-to-vehicle electricity sharing platform service',
+      startDate: 'Dec 2022',
+      endDate: 'Feb 2023',
+      link: '',
+      description: [
+        'Collaborated in a cross-functional team to develop a car-sharing solution using a map API for locating nearby vehicles.',
+        'Designed and implemented responsive React components to enhance user experience.',
+        'Contributed to the review registration and feedback feature for user engagement.',
+        'Focused on user-centered UI design principles.',
+      ],
+      technologies: ['React', 'React-query', 'Figma'],
+    },
+  ],
+  education: [
+    {
+      institution: 'Incheon National University',
+      degree: 'Bachelor of Computer Science',
+      startDate: 'Mar 2022',
+      endDate: 'Present',
+      gpa: '',
+      logo: '',
+    },
+  ],
+  certifications: [
+    {
+      name: 'Machine Learning Specialization',
+      organization: 'Deeplearning.AI',
+      dateEarned: 'Dec 2024',
+      validUntil: '',
+      link: 'https://coursera.org/share/e602ff745bdf81641b96c76f96cb36a0',
+    },
+    {
+      name: 'AWS Certified Cloud Practitioner',
+      organization: 'Amazon Web Services',
+      dateEarned: 'Oct 2024',
+      validUntil: '',
+      link: 'https://www.credly.com/badges/7a04f09e-5e11-42b6-838e-ee5af58ec955/public_url',
+    },
+  ],
 };
 
 module.exports = resumeData;
